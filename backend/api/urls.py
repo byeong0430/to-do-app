ad5d1289a698d7a09068fcb3f63f95ebb4b02ed4
byeong0430 <byeong.kim0430@gmail.com>
2019-02-01 17:01:37 +0000

    Add category url

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('tasks', views.task_list, name='task')
+    path('tasks', views.task_list, name='task'),
+    path('categories', views.category_list, name='category')
 ]
