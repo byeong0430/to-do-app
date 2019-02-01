7e173a4ba816f97feb8b1d4671a760559fc0f0ff
byeong0430 <byeong.kim0430@gmail.com>
2019-02-01 23:11:24 +0000

    Set url paths

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('tasks', views.task_list, name='task'),
-    path('categories', views.category_list, name='category')
+    path('categories', views.CategoryList.as_view(), name='category_list'),
+    path('categories/<int:category_id>', views.CategoryDetail.as_view(), name='category_detail'),
+    path('categories/<int:category_id>/tasks', views.CategoryTaskList.as_view(), name='category_task_list'),
+    path('tasks', views.TaskList.as_view(), name='task_list'),
+    path('tasks/<int:task_id>', views.TaskDetail.as_view(), name='task_detail')
 ]
