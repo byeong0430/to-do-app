33ceff676621ca536f26b11a7341f2ab54d23a86
byeong0430 <byeong.kim0430@gmail.com>
2019-02-01 23:11:08 +0000

    Add the rest of api endpoints

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,52 +1,109 @@
-from django.shortcuts import render
-from django.views.decorators.csrf import csrf_exempt
+from django.http import Http404
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
-from rest_framework.parsers import JSONParser
+from rest_framework.views import APIView
 from rest_framework import status
 from .models import *
 from .serializers import *
 
-@csrf_exempt
-@api_view(['GET', 'POST'])
-# Create your views here.
-def task_list(request):
-    '''
-    List tasks or create a new task
-    '''
-    if request.method == 'GET':
-        tasks = Task.objects.all()
-        serializer = TaskSerializer(
-            tasks,
-            context={'request', request},
-            many=True
-        )
-        return Response(serializer.data)
-    elif request.method == 'POST':
-        serializer = TaskSerializer(data=request.POST)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(
-                serializer.data,
-                status.HTTP_201_CREATED
-                )
-        return Response(
-            serializer.data,
-            status.HTTP_400_BAD_REQUEST
-            )
 
-@csrf_exempt
-@api_view(['GET', 'POST'])
-def category_list(request):
-    if request.method == 'POST':
+def get_object(Model, pk):
+    '''
+    Retrieve an object based on its pk
+    '''
+    try:
+        return Model.objects.get(pk=pk)
+    except Model.DoesNotExist:
+        raise Http404
+
+class CategoryList(APIView):
+    '''
+    Retrieve a list of categories or add a category
+    '''
+    def get(self, request, format=None):
+        categories = Category.objects.all()
+        serializer = CategorySerializer(categories, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
         serializer = CategorySerializer(data=request.POST)
         if serializer.is_valid():
             serializer.save()
-            return Response(
-                serializer.data,
-                status.HTTP_201_CREATED
-                )
-        return Response(
-            serializer.data,
-            status.HTTP_400_BAD_REQUEST
-            )
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class CategoryDetail(APIView):
+    '''
+    Retrieve, update, or delete a category instance
+    '''
+    def get(self, request, category_id, format=None):
+        category = get_object(Category, category_id)
+        serializer = CategorySerializer(category)
+        return Response(serializer.data)
+
+    def put(self, request, category_id, format=None):
+        category = get_object(Category, category_id)
+        serializer = CategorySerializer(category, data=request.POST, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, category_id, format=None):
+        category = get_object(Category, category_id)
+        category.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class CategoryTaskList(APIView):
+    '''
+    Retrieve a list of tasks within a category
+    '''
+    def get(self, request, category_id, format=None):
+        tasks = Task.objects.filter(category_id=category_id)
+        serializer = TaskSerializer(tasks, many=True)
+        return Response(serializer.data)
+
+
+class TaskList(APIView):
+    '''
+    Retrieve a list of tasks or add a task
+    '''
+    def get(self, request, format=None):
+        tasks = Task.objects.all()
+        serializer = TaskSerializer(tasks, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = TaskSerializer(data=request.POST)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class TaskDetail(APIView):
+    '''
+    Retrieve, update, or delete a task
+    '''
+    def get(self, request, task_id, format=None):
+        task = get_object(Task, task_id)
+        serializer = TaskSerializer(task)
+        return Response(serializer.data)
+
+    # Set partial=True to allow partial change
+    def put(self, request, task_id, format=None):
+        task = get_object(Task, task_id)
+        serializer = TaskSerializer(task, data=request.POST, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, task_id, format=None):
+        task = get_object(Task, task_id)
+        task.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
