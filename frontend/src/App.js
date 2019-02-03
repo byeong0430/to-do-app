1ef9c550032a2b1758e267cff99e6a5e4628e46f
byeong0430 <byeong.kim0430@gmail.com>
2019-02-03 23:46:05 +0000

    Map thunk function to component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,19 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+import { connect } from 'react-redux';
 import './App.css';
 
+import { handleGetTaskList } from './redux/thunks';
+
 class App extends Component {
-  getTest = async () => {
-    const instance = axios.create({
-      baseURL: 'http://byeongkim.ca:8000/api/',
-      timeout: 1000
-    });
-
-    try {
-      const response = await instance.get('/tasks');
-      console.log(response);
-    } catch (error) {
-      console.log(error);
-      console.log(error.request);
-      console.log(error.response);
-    }
-  }
-
   render() {
     return (
-      <button onClick={this.getTest}>GET</button>
+      <button onClick={this.props.handleGetTaskList}>
+        GET
+      </button>
     );
   }
 }
 
-export default App;
+const mapDispatchToProps = { handleGetTaskList }
+
+export default connect(null, mapDispatchToProps)(App);
