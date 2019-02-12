5d963b29c58c2a431de2fb6fd6c9f52047dd9fe7
byeong0430 <byeong.kim0430@gmail.com>
2019-02-12 22:03:50 +0000

    Add get category list action

--- frontend/src/redux/actions/taskActions.js
+++ frontend/src/redux/actions/taskActions.js
@@ -1,6 +1,13 @@
-export const getTaskList = taskList => {
+export const saveTaskList = taskList => {
   return {
     type: 'GET_TASK_LIST',
     payload: taskList
   }
 };
+
+export const saveCatList = catList => {
+  return {
+    type: 'GET_CAT_LIST',
+    payload: catList
+  }
+}
