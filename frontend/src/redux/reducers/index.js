07af053f1d0f76f9e9f71690775c019ed4557df4
byeong0430 <byeong.kim0430@gmail.com>
2019-02-12 22:03:14 +0000

    Add category reducer

--- frontend/src/redux/reducers/index.js
+++ frontend/src/redux/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import { taskReducer } from './taskReducer';
+import { categoryReducer } from './categoryReducer';
 
 export default combineReducers({
-  task: taskReducer
+  task: taskReducer,
+  category: categoryReducer
 })
