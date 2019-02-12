6ff6719ac2f94a317181aa16e697c69a3e247bf9
byeong0430 <byeong.kim0430@gmail.com>
2019-02-12 22:05:33 +0000

    Add new components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,25 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import './App.css';
+import SideBar from './components/SideBar';
+import MainMenu from './components/MainMenu';
 
 import { handleGetTaskList } from './redux/thunks';
 
 class App extends Component {
   render() {
     return (
+      <>
+      <div className='container'>
+        <div className='row'>
+          <SideBar/>
+          <MainMenu/>
+        </div>
+      </div>
       <button onClick={this.props.handleGetTaskList}>
         GET
       </button>
+      </>
     );
   }
 }
